c1bdc8277183b44635c5c275134507bf103cbe2d
arslonga83 <jlwood83@gmail.com>
2022-08-27 21:50:31 -0700

    add comments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+//things to do....
+//add a clear function that removes crossed off items/projects
+//divide up some functions that are doing too many things....divide between dom and data
+//add style!
+//research, add date library 
+
+
+
 import toDo, {projects} from "./todo.js";
 import {newItem, newProject, buttons, printProjects, populateForm} from "./DOMstuff.js";
 
